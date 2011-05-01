4614b1c24c7c82068b5dff271616c71d53423f28
7rans <transfire@gmail.com>
2011-05-01 17:42:07 -0400

    admin: give Assay module access to metadata

--- lib/assay.rb
+++ lib/assay.rb
@@ -1,10 +1,43 @@
 module Assay
   VERSION="1.0.0"
+
+  # Returns a Hash table of failure classes indexed by
+  # asserton operator.
+  def self.failure_classes_by_operator
+    @_failure_classes_by_operator ||= (
+      c = {}
+      ObjectSpace.each_object(Class) do |fc|
+        next unless fc < Assay::Failure
+        if fc.respond_to?(:assertion_operator)
+          c[fc.assertion_operator.to_sym] = fc
+        end
+      end
+      c
+    )
+  end
+
+  # Lookup failure class by operator.
+  def self.lookup(operator)
+    failure_classes_by_operator[operator.to_sym]
+  end
+
+  # Returns Hash table of project metadata.
+  def self.meta
+    @spec ||= (
+      require 'yaml'
+      YAML.load(File.new(File.dirname(__FILE__) + '/ansi.yml'))
+    )
+  end
+
+  # Check metadata for missing constants.
+  def self.const_missing(name)
+    meta[name.to_s.downcase] || super(name)
+  end
 end
 
+# Load Assay's failure classes.
 dir  = File.dirname(__FILE__)
 glob = File.join(dir, 'assay', '*.rb')
-
 Dir[glob].each do |rb|
   require 'assay/' + File.basename(rb)
 end
