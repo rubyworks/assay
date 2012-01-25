75ece635954e5b385ca23bda5c322fb449b844ba
7rans <transfire@gmail.com>
2012-01-25 14:52:22 -0500

    Ratio comparison for WithinAssay does not work.

--- lib/assay/within_assay.rb
+++ lib/assay/within_assay.rb
@@ -11,29 +11,27 @@ class WithinAssay < CompareAssay
   # Check assertion.
   #
   def self.pass?(subject, criterion, delta)
-    if Numeric === delta
-      subject   = subject.to_f
-      criterion = criterion.to_f
-      delta     = delta.to_f
+    if [subject, criterion, delta].all?{ |v| Numeric === v }
+      a, b, d = subject.to_f, criterion.to_f, delta.to_f
+    else
+      a, b, d = subject, criterion, delta
     end
 
-    a, c, d = subject, criterion, delta
-
-    (c - d) <= a && (c + d) >= a
+    (b - d) <= a && (b + d) >= a
   end
 
   #
   # Failed assertion message.
   #
   def self.assert_message(subject, criterion, delta)
-    x = subject.inspect
-    y = criterion.inspect
+    a = subject.inspect
+    b = criterion.inspect
     d = delta.inspect
 
-    if [x, y, d].any?{ |e| e.size > SIZE_LIMIT }
-      "y - d <= x <= y + d\nx) #{x}\ny) #{y}\nd) #{d}"
+    if [a, b, d].any?{ |e| e.size > SIZE_LIMIT }
+      "b - d <= a <= b + d\na) #{a}\nb) #{b}\nd) #{d}"
     else
-      "#{y} - #{d} <= #{x} <= #{x} + #{d}"
+      "#{b} - #{d} <= #{a} <= #{b} + #{d}"
     end
   end
 
