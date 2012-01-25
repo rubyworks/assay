76f583478a82dacbd10d474699ff0509da2608b6
7rans <transfire@gmail.com>
2012-01-25 12:19:04 -0500

    Clean-up Assertor code and fix its tests. [minor]

--- lib/assay/assertor.rb
+++ lib/assay/assertor.rb
@@ -16,6 +16,28 @@ module Assay
     end
 
     #
+    # The assay class to which this assertor delegates.
+    #
+    attr :assay
+
+    #
+    # The criteria for applying the assertor.
+    #
+    attr :criteria
+
+    #
+    # Block criterion.
+    #
+    attr :block
+
+    #    
+    # Is the assertor negated?
+    #
+    def not?
+      @not
+    end
+
+    #
     #
     def pass?(subject, &block)
       arguments, block = complete_criteria(subject, &block)
@@ -92,17 +114,19 @@ module Assay
     end
 
     #
-    #
+    # Assertion message. This is only used by RSpec compatibility methods.
     #
     def assert_message(subject, &block)
-      @assay.assert_message(subject, *criteria, &block)
+      arguments, block = complete_criteria(subject, &block)
+      @assay.assert_message(*arguments, &block)
     end
 
     #
-    #
+    # Refutation message. This is only used by RSpec compatibility methods.
     #
     def refute_message(subject, &block)
-      @assay.refute_message(subject, *criteria, &block)
+      arguments, block = complete_criteria(subject, &block)
+      @assay.refute_message(*arguments, &block)
     end
 
     # The following methods allow Assay objects to work as RSpec matchers.
@@ -118,6 +142,15 @@ module Assay
 
     # For RSpec matcher compatability.
     alias failure_message_for_should_not refute_message
+
+    ##
+    ## Returns Assay instance.
+    ##
+    #def exception(subject, msg=nil)
+    #  @assay.new(msg || message, subject, *criteria, &block)
+    #  #  :negated   => options[:negated],
+    #  #  :backtrace => options[:backtrace] || caller,
+    #end
 
   protected
 
@@ -151,168 +184,4 @@ module Assay
   end
 
 end
-
-
-
-
-
-
-
-
-
-=begin
-
-  # Assertion Matcher is used to convert an Assertion class into an 
-  # Matcher class.
-  #
-  # TODO: Better name for this class ?
-  #
-  class Matcher
-
-    #
-    def initialize(assertion, *criteria, &block)
-      @assertion = assertion
-      @criteria = criteria
-      @block     = block
-    end
-
-    #
-    def assertion
-      @assertion
-    end
-
-    #
-    def pass?(subject)
-      if Proc === subject && @block.nil?
-        @assertion.pass?(*@criteria, &subject)
-      else
-        @assertion.pass?(*complete_criteria(subject), &@block)
-      end
-    end
-
-    #
-    def fail?(subject)
-      if Proc === subject && @block.nil?
-        @assertion.fail?(*@criteria, &subject)
-      else
-        @assertion.fail?(*complete_criteria(subject), &@block)
-      end
-    end
-
-    #
-    def pass!(subject)
-      if Proc === subject && @block.nil?
-        @assertion.pass!(*@criteria, &subject)
-      else
-        @assertion.pass!(*complete_criteria(subject), &@block)
-      end
-    end
-
-    #
-    def fail!(subject)
-      if Proc === subject && @block.nil?
-        @assertion.fail!(*@criteria, &subject)
-      else
-        @assertion.fail!(*complete_criteria(subject), &@block)
-      end
-    end
-
-    alias_method :==, :pass?
-    alias_method :!=, :fail?
-
-    alias_method :=~, :pass!
-    alias_method :!~, :fail!
-
-    alias_method :===, :pass!
-
-    #
-    def pass_message(subject)
-      @assertion.pass_message(*complete_criteria(subject))
-    end
-
-    #
-    def fail_message(subject)
-      @assertion.fail_message(*complete_criteria(subject))
-    end
-
-    # For RSpec matcher compatability.
-    alias matches? pass?
-
-    # For RSpec matcher compatability.
-    alias does_not_match? fail?
-
-    # For RSpec matcher compatability.
-    alias failure_message_for_should pass_message
-
-    # For RSpec matcher compatability.
-    alias failure_message_for_should_not fail_message
-
-    ##
-    ## Returns Exception instance.
-    ##
-    #def exception(subject, msg=nil)
-    #  @assertion.new(msg || message, subject, *@criteria, &@block)     
-    #  #  :negated   => options[:negated],
-    #  #  :backtrace => options[:backtrace] || caller,
-    #end
-
-    ##
-    #def fail(*args)
-    #  super(self, *args)
-    #end
-
-    # Create a negated form of the matcher.
-    #
-    # @todo Should this be @! method instead?
-    #
-    def ~@
-      Negated.new(self)
-    end
-
-    # Create a negated form of the matcher.
-    #
-    # @todo Best name for this method?
-    #
-    def negated
-      Negated.new(self)
-    end
-
-  private
-
-    def complete_criteria(subject)
-      if i = @criteria.index(__)
-        @criteria[0...i] + [subject] + @criteria[i+1..-1]
-      else
-        [subject] +  @criteria
-      end
-    end
-
-    # Negated Matcher
-    #
-    class Negated
-      def initialize(matcher)
-        @matcher = matcher
-      end
-
-      def pass?(*a,&b) ; @matcher.fail?(*a,&b) ; end
-      def fail?(*a,&b) ; @matcher.pass?(*a,&b) ; end
-      def pass!(*a,&b) ; @matcher.fail!(*a,&b) ; end
-      def fail!(*a,&b) ; @matcher.pass!(*a,&b) ; end
-
-      alias_method :==, :pass?
-      alias_method :!=, :fail?
-
-      alias_method :=~, :pass!
-      alias_method :!~, :fail!
-
-      alias_method :===, :pass!
-
-      def method_missing(s, *a, &b)
-        @matcher.send(s, *a, &b)
-      end
-    end
-
-  end
-
-=end
 
---
+++ spec/03_assertor.md
@@ -0,0 +1,100 @@
+# Assay as Assertor
+
+Assay classes can be converted to assertors, otherwise known as *matchers* in
+RSpec circles. The idea behind Assertors is that they can be initialized with
+pre-set criteria and then applied to target subjects.
+
+## Standard Target Matching
+
+    assertor = EqualAssay[1]
+
+    assertor.assert!(1)
+
+    expect EqualAssay do
+      assertor.assert!(2)
+    end
+
+## Partial Arguments
+
+    assertor = LessAssay[1,__]
+
+We can apply the assertor using the `#pass!` method.
+
+    assertor.assert!(2)
+
+Likewise we can assert the negated expression using `#fail!`.
+
+    assertor.refute!(0)
+
+Assay partial a very versile because they allow any argument to become
+the target of a assertor.
+
+    assertor = CompareAssay[2,1,__]
+
+    assertor.assert!(1)
+    assertor.refute!(0)
+
+## Match Operator
+
+The `#=~` method is an alias for `#assert!`.
+
+    assertor = EqualAssay[1]
+
+    assertor =~ 1
+
+Conversely, `#!~` method is an alias for `#refute!`.
+
+    assertor !~ 2
+
+## Case Operator
+
+The `#===` method is also an alias for `#assert!`.
+
+    assertor === 1
+
+This allows the `case` statement to be used in an intersting way.
+
+    case 10
+    when KindAssay[Numeric]
+    when EqualAssay[10.0]
+    end
+
+## Negated Matchers
+
+Matchers can be negated so that pass and fail methods swap behaviors.
+
+    assertor = EqualityAssay[__,1]
+
+    assertor.negated =~ 2
+    assertor.negated !~ 1
+
+For conveience we can also negate the assertor using the `~` unary operator.
+
+    ~assertor === 2
+
+## Just Cheking
+
+Assertors can also be used just to test the assertion without actually 
+raising the associated exception.
+
+    assertor = EqualityAssay[__,1]
+
+    assertor.pass?(1)
+    assertor.fail?(2)
+
+## RSpec Compatability
+
+RSpec compatibality is provided via neccessary method alias, in particular
+`#matches?` and `#does_not_match?`.
+
+    assertor = EqualityAssay[__,1]
+
+    assert assertor.matches?(1)
+    assert assertor.does_not_match?(2)
+
+Error messages are handle by RSpec by `#failure_message_for_should` and
+`#failure_message_for_should_not`.
+
+    assertor.failure_message_for_should(1)
+    assertor.failure_message_for_should_not(2)
+
